677f20e8c83b0e686986919594a97d37e4c4dafb
mikromike <dev@mikromike.email>
2023-09-04 14:51:47 +0300

    Filament setup part 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -11,8 +12,16 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
+
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @filamentStyles
+        @vite(['resources/css/app.css'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -30,6 +39,9 @@
             <!-- Page Content -->
             <main>
                 {{ $slot }}
+
+                @filamentScripts
+                @vite('resources/js/app.js')
             </main>
         </div>
     </body>
